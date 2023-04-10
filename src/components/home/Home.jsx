8a5e27ec011759e0ace41c1b9f94aec387aad1e5
imran-h-ob <imran.h.ovi.61@gmail.com>
2023-04-10 14:02:54 +0600

    json data added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,55 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h2>home page</h2>
+      {/* cover section */}
+      <main>
+        <div className="cover">
+          <div className="cover-text">
+            <h2>
+              One Step Closer To Your
+              <span> Dream Job</span>
+            </h2>
+            <p>
+              Finding the right job made easy with Higher Hires. Register Now
+              and Apply to the best jobs! Get Headhunted By Top Recruiters and
+              Find the Right Job On Higher Hires.
+            </p>
+            <button>Get Started</button>
+          </div>
+          <div className="cover-img">
+            <img
+              src="https://i.ibb.co/8dbhF6R/businessman-11.png"
+              alt="businessman-11"
+            />
+          </div>
+        </div>
+      </main>
+
+      {/* job category section */}
+      <div className="categories-container">
+        <div className="section-title">
+          <h2>Job Category List</h2>
+          <p>We are providng all the IT relatives jobs. Find you're one</p>
+        </div>
+      </div>
+
+      {/* job category section */}
+      <div className="categories-container">
+        <div className="section-title">
+          <h2>Featured Jobs</h2>
+          <p>
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+      </div>
+
+      {/*  */}
     </div>
   );
 };
 
 export default Home;
-
-// https://ibb.co/9h1KnmX
